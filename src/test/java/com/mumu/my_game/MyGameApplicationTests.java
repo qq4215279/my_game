93dafd3df9117f0ecbe342299cb7cd8daf7c8b28
liuzhen <liu865488158@qq.com>
2025-02-11 20:32:26 +0800

    Initial commit

---
+++ src/test/java/com/mumu/my_game/MyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumu.my_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
