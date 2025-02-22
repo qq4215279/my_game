9a7f785a742897820b2b5f0f34cf0576a84c44e7
qq4215279 <liu865488158@qq.com>
2025-02-22 22:42:08 +0800

    add dependencies

---
+++ game/src/main/java/com/mumu/game/GameApplication.java
@@ -0,0 +1,17 @@
+package com.mumu.game;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * GameApplication
+ * @author liuzhen
+ * @version 1.0.0 2025/2/22 15:02
+ */
+@SpringBootApplication
+public class GameApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameApplication.class, args);
+  }
+
+}
---
+++ gateway/src/main/java/com/mumu/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.mumu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GatewayApplication
+ * @author liuzhen
+ * @version 1.0.0 2025/2/22 15:13
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+
+}
